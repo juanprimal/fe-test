f18073716ea48b440f58ed2887428853d07a4cb1
Pedro Juan Criollo Romo <pedrojuancr@gmail.com>
2018-04-15 07:41:37 -0500

    Automated commit at 2018-04-15 12:41:37 UTC by middleman-deploy 2.0.0-alpha

---
+++ javascripts/site.js
@@ -0,0 +1,44 @@
+$(function () {
+
+  //if click or tab outside menu closes main-menu
+  $(window).bind("click touchstart", function(event){
+    if(!$(event.target).closest('.main-menu').length) {
+      $('.dropdown').removeClass('toggled');
+    }
+  });
+
+  //togle menu when click on hamburguer buttons
+  $('body').on('click', '.toggle-menu', function(event) {
+    var targetElement = $(this).attr("href");
+    event.stopPropagation();
+    event.preventDefault();
+    $(targetElement).toggleClass('toggled');
+  });
+
+  //tabs extra content for CTAs
+  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+    var tabIndex = $(e.target).closest('li').index();
+    $('.tab-content-extra .tab-pane').removeClass('active');
+    $('.tab-content-extra .tab-pane').eq(tabIndex).addClass('active');
+  });
+
+  //this code is just to load links url faster via ajax and without reloads
+  //if don't want to use it just comment
+  $('body').on('click', '.dropdown .dropdown__link', function(event) {
+    event.preventDefault();
+    var targetElement = $(this).attr("href");
+
+    $.ajax({
+      url: targetElement,
+      dataType: "text",
+      success: function(data) {
+
+        var body = data.substring(data.indexOf("<body>")+6,data.indexOf("</body>"));
+        $('body').hide().html(body).fadeIn('fast');
+      }
+    });
+
+  });
+  //end of reload
+
+});
